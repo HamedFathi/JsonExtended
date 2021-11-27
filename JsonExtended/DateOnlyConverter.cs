ab72f0266a6160773bf444784ffc76eb574a068b
hamedfathi <hamedfathi@outlook.com>
2021-11-27 22:08:06 +0330

    feat: DateOnly & TimeOnly converters added.

---
+++ JsonExtended/DateOnlyConverter.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+#if NET6_0_OR_GREATER
+
+using System;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace JsonExtended
+{
+    // var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+    // options.Converters.Add(new DateOnlyConverter());
+    // options.Converters.Add(new TimeOnlyConverter());
+
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        private readonly string serializationFormat;
+
+        public DateOnlyConverter() : this(null)
+        {
+        }
+
+        public DateOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "yyyy-MM-dd";
+        }
+
+        public override DateOnly Read(ref Utf8JsonReader reader,
+                                Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return DateOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value,
+                                            JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+}
+
+#endif
---
+++ JsonExtended/TimeOnlyConverter.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+#if NET6_0_OR_GREATER
+
+using System;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace JsonExtended
+{
+    // var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+    // options.Converters.Add(new DateOnlyConverter());
+    // options.Converters.Add(new TimeOnlyConverter());
+
+    public class TimeOnlyConverter : JsonConverter<TimeOnly>
+    {
+        private readonly string serializationFormat;
+
+        public TimeOnlyConverter() : this(null)
+        {
+        }
+
+        public TimeOnlyConverter(string? serializationFormat)
+        {
+            this.serializationFormat = serializationFormat ?? "HH:mm:ss.fff";
+        }
+
+        public override TimeOnly Read(ref Utf8JsonReader reader,
+                                Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return TimeOnly.Parse(value!);
+        }
+
+        public override void Write(Utf8JsonWriter writer, TimeOnly value,
+                                            JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToString(serializationFormat));
+    }
+}
+
+#endif
